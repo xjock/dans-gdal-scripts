52026bf0205f56f4cf6c1e86cb84f3c4dcc19b52
Luca Guo <guoruiuser@163.com>
2026-05-07 09:12:34 +0800

    修复不过

---
+++ confdefs.h
@@ -0,0 +1,15 @@
+/* confdefs.h */
+#define PACKAGE_NAME "dans-gdal-scripts"
+#define PACKAGE_TARNAME "dans-gdal-scripts"
+#define PACKAGE_VERSION "0.24"
+#define PACKAGE_STRING "dans-gdal-scripts 0.24"
+#define PACKAGE_BUGREPORT "dan@stahlke.org"
+#define PACKAGE_URL ""
+#define PACKAGE "dans-gdal-scripts"
+#define VERSION "0.24"
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRINGS_H 1
---
+++ conftest.cpp
@@ -0,0 +1,45 @@
+/* confdefs.h */
+#define PACKAGE_NAME "dans-gdal-scripts"
+#define PACKAGE_TARNAME "dans-gdal-scripts"
+#define PACKAGE_VERSION "0.24"
+#define PACKAGE_STRING "dans-gdal-scripts 0.24"
+#define PACKAGE_BUGREPORT "dan@stahlke.org"
+#define PACKAGE_URL ""
+#define PACKAGE "dans-gdal-scripts"
+#define VERSION "0.24"
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRINGS_H 1
+/* end confdefs.h.  */
+#include <stddef.h>
+#ifdef HAVE_STDIO_H
+# include <stdio.h>
+#endif
+#ifdef HAVE_STDLIB_H
+# include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+#ifdef HAVE_INTTYPES_H
+# include <inttypes.h>
+#endif
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+#include <sys/stat.h>
